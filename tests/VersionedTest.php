83572f4cc84c3d3a9a855c582d6916069064d6ab
Sebastien Barre <sbarre@gmail.com>
2015-05-24 22:22:05 -0400

    added first successful unit tests!

--- tests/VersionedTest.php
+++ tests/VersionedTest.php
@@ -23,9 +23,31 @@ class VersionedTest extends FunctionalTestCase
     public function testCreateModel($data)
     {
         $className = $this->modelPrefix.$data['name'];
-        $object = $className::create($data);
-        return true;
+	    $model = $className::create($data)->fresh();
+
+	    $this->assertInstanceOf('Illuminate\\Database\\Eloquent\\Model', $model);
+	    $this->assertEquals(1, $model->id);
+	    $this->assertEquals(1, $model->version);
+	    $this->assertEquals(1, $model->is_current_version);
     }
+
+	/**
+	 * @depends testCreateModel
+	 * @dataProvider createDataProvider
+	 */
+	public function testVersioningModel($data)
+	{
+		$className = $this->modelPrefix.$data['name'];
+		$model = $className::create($data)->fresh();
+
+		$model->name = 'Updated Widget';
+		$model->save();
+
+		$this->assertEquals(1, $model->id);
+		$this->assertEquals(2, $model->version);
+		$this->assertEquals(1, $model->is_current_version);
+
+	}
 
     /**
      * Provides objects to use by tests
@@ -35,11 +57,9 @@ class VersionedTest extends FunctionalTestCase
     public function createDataProvider()
     {
         return array(
-            array(
-                array('name' => 'Widget', 'gadget_id' => 1, 'doodad_id' => 1),
-                array('name' => 'Gadget', 'widget_id' => 1, 'doodad_id' => 1),
-                array('name' => 'Doodad', 'widget_id' => 1, 'gadget_id' => 1)
-            )
+	        array(array('name' => 'Widget', 'gadget_id' => 1, 'doodad_id' => 1)),
+	        array(array('name' => 'Gadget', 'widget_id' => 1, 'doodad_id' => 1)),
+	        array(array('name' => 'Doodad', 'widget_id' => 1, 'gadget_id' => 1))
         );
     }
 
